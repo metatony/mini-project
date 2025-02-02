1bf18f8067c39d184104ca00cc0813f1d6b63615
metatony <ibegbulam.ua@gmail.com>
2025-02-02 16:54:11 +0000

    added functionality to make the code rotate in reverse direction on click

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [rotateDirection, setRotateDirection] = useState(1); // used 1 for clockwise and -1 for anti-clockwise
+
+  const handleClick = () => {
+    setRotateDirection((prev) => prev * -1); //reverse direction on when icon is clicked
+  };
   return (
     <>
       <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a>
+          <img
+            src={reactLogo}
+            className="logo react"
+            alt="React logo"
+            style={{
+              transform: `rotate(${rotateDirection * 90}deg)`,
+              transition: "transform 1s",
+            }}
+            onClick={handleClick}
+          />
         </a>
       </div>
     </>
