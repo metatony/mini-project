14a8a69cd95e73879db7880693b238758011f4d1
metatony <ibegbulam.ua@gmail.com>
2025-02-02 17:34:43 +0000

    added functionality to adjust the react icon size based on cursor position

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,36 @@ import { useState } from "react";
 
 function App() {
   const [rotateDirection, setRotateDirection] = useState(1); // used 1 for clockwise and -1 for anti-clockwise
+  const [iconSize, setIconSize] = useState(100); // default icon size
 
   const handleClick = () => {
-    setRotateDirection((prev) => prev * -1); //reverse direction on when icon is clicked
+    setRotateDirection((prev) => prev * -1); // this function reverses the rotation direction when icon is clicked
+  };
+
+  const handleMouseMove = (e) => {
+    const { clientX, clientY } = e;
+    const newSize = Math.max(50, Math.min(200, (clientX + clientY) / 8)); // adjust icon size based on cursor position
+
+    setIconSize(newSize);
   };
   return (
-    <>
+    <div onMouseMove={handleMouseMove}>
       <div>
-        <a>
-          <img
-            src={reactLogo}
-            className="logo react"
-            alt="React logo"
-            style={{
-              transform: `rotate(${rotateDirection * 90}deg)`,
-              transition: "transform 1s",
-            }}
-            onClick={handleClick}
-          />
-        </a>
+        <img
+          src={reactLogo}
+          className="logo react"
+          alt="React logo"
+          style={{
+            transform: `rotate(${rotateDirection * 90}deg)`,
+            transition: "transform 1s",
+
+            width: `${iconSize}px`,
+            height: `${iconSize}px`,
+          }}
+          onClick={handleClick}
+        />
       </div>
-    </>
+    </div>
   );
 }
 
